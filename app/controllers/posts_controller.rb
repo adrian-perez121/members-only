6091897b4f7746f11f4a8dbeb464121d8d8f0ed4
Adrian Perez <adrian1804perez@gmail.com>
2024-07-24 14:46:29 -0700

    Add new and create actions and view for Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,28 @@
 class PostsController < ApplicationController
-  before_action :user_signed_in?
+  before_action :authenticate_user!, only: [:new, :create]
 
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
   def index
     @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 
 end
